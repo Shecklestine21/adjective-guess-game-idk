629fadc93e774f4959a767c3cf259a24b437bb4f
Shecklestine21 <145370753+Shecklestine21@users.noreply.github.com>
2023-11-20 09:49:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+document.getElementById("btn1").addEventListener("click", idiom1);
+
+function idiom1() {
+    alert("bullet")
+}
+
+
+document.getElementById("btn2").addEventListener("click", idiom2);
+
+function idiom2() {
+    alert("having fun")
+}
+
+
+document.getElementById("btn3").addEventListener("click", idiom3);
+
+function idiom3() {
+    alert("matters worse")
+}
+
+
+document.getElementById("btn4").addEventListener("click", idiom4);
+
+function idiom4() {
+    alert("good as mine")
+}
+
+
+
+document.getElementById("btn5").addEventListener("click", idiom5);
+
+function idiom5() {
+    alert("no gain")
+}
